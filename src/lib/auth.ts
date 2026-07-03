7fa98799e4de4fd40ed580d8b2f823f94d108923
Rajat0741 <kushwaharajat1255@gmail.com>
2026-07-03 13:19:00 +0530

    fix(auth): add beter auth api key to dash plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,27 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
-import { admin, oneTap } from "better-auth/plugins"; 
-import { user,account, session, verification } from "./db/schema";
+import { admin, oneTap } from "better-auth/plugins";
+import { user, account, session, verification } from "./db/schema";
 import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
-    appName: "SparkVoid",
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema: { user, account, session, verification },
-    }),
-    socialProviders: {
-        google: {
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }
+  appName: "SparkVoid",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: { user, account, session, verification },
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    plugins: [
-        oneTap(),
-        dash(),
-        admin()
-    ]
+  },
+  plugins: [
+    oneTap(),
+    dash({
+      apiKey: process.env.BETTER_AUTH_API_KEY,
+    }),
+    admin(),
+  ],
 });
